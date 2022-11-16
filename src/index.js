08c29dd0e74a3448ac0b58b6e4f78a4aad6b4a4a
Júlia de Castro <juliacastro@cpejr.com.br>
2022-11-15 22:12:49 -0300

    Instalando ws e conectando com um servidor

--- src/index.js
+++ src/index.js
@@ -1,6 +1,28 @@
 const express = require('express');
 const cors = require('cors');
 
+// const readlineSync = require('readline-sync');
+// const symbol = readlineSync.question('Qual par de moedas deseja monitorar? ');
+
+const WebSocket = require('ws');
+const ws = new WebSocket('wss://stream.binance.com:9443/ws/btcbrl@bookTicker');
+
+// ws.onopen = () => {
+// ws.send(JSON.stringify({
+//    "method": "SUBSCRIBE",
+//    "params": [`${symbol}@bookTicker`],
+//    "id": 1
+//    }))
+// }
+
+ws.onmessage = (event) => {
+  process.stdout.write('\033c');
+  const ob = JSON.parse(event.data);
+  console.log(`Symbol: ${ob.s}`);
+  console.log(`Best ask: ${ob.a}`)
+  console.log(`Best bit: ${ob.b}`);
+}
+
 const port = 3333;
 
 const app = express();
