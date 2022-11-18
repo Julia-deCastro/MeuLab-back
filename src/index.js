ea11936894c3ff779875477cca9097e2a8185fd7
Júlia de Castro <juliacastro@cpejr.com.br>
2022-11-18 16:33:24 -0300

    Criando server socket local

--- src/index.js
+++ src/index.js
@@ -1,35 +1,28 @@
-const express = require('express');
-const cors = require('cors');
+const Koa = require('koa')
+const http = require('http')
+const socket = require('socket.io')
 
-// const readlineSync = require('readline-sync');
-// const symbol = readlineSync.question('Qual par de moedas deseja monitorar? ');
-
-const WebSocket = require('ws');
-const ws = new WebSocket('wss://stream.binance.com:9443/ws/btcbrl@bookTicker');
-
-// ws.onopen = () => {
-// ws.send(JSON.stringify({
-//    "method": "SUBSCRIBE",
-//    "params": [`${symbol}@bookTicker`],
-//    "id": 1
-//    }))
-// }
-
-ws.onmessage = (event) => {
-  process.stdout.write('\033c');
-  const ob = JSON.parse(event.data);
-  console.log(`Symbol: ${ob.s}`);
-  console.log(`Best ask: ${ob.a}`)
-  console.log(`Best bit: ${ob.b}`);
-}
-
-const port = 3333;
-
-const app = express();
-
-app.use(cors());
-app.use(express.json());
-
-app.listen(port, () => {
-  console.log('Server listening on port: ' + port);
+const app = new Koa()
+const server = http.createServer(app.callback())
+const io = socket(server,{
+  cors:{
+      origin:'*'
+  }
 })
+
+const SERVER_HOST = 'localhost'
+const SERVER_PORT = 8080
+
+io.on('connection', socket => {
+    console.log('[IO] Connection => Server has a new connection')
+    socket.on('message', data => {
+        console.log(data)
+        io.emit('message', data)
+    })
+    socket.on('disconnect', () => {
+        console.log('[SOCKET] Disconnect => A connection was disconnected')
+    })
+})
+
+server.listen(SERVER_PORT, SERVER_HOST, () => {
+    console.log(`Server is running at http://${SERVER_HOST}:${SERVER_PORT}`)})
