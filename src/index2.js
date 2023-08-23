67e2a04707496a23d155f8fe74e4dad625c52a66
Júlia de Castro <juliadecastro777@gmail.com>
2023-08-23 16:32:28 -0300

    atualização ssl

--- src/index2.js
+++ src/index2.js
@@ -5,11 +5,23 @@ const { errors } = require('celebrate');
 const swaggerUi = require('swagger-ui-express');
 const swaggerJsDoc = require('swagger-jsdoc');
 const routes = require('./routes');
+const https = require('https');
+const fs = require('fs');
 
-const port = process.env.PORT || 3333;
+const port = process.env.PORT || 3347;
 
 const app = express();
-app.use(express.static('public'));
+
+// Carregue os certificados
+const privateKey = fs.readFileSync('../../certs/privkey.pem', 'utf8');
+const certificate = fs.readFileSync('../../certs/cert.pem', 'utf8');
+//const ca = fs.readFileSync('caminho_para_ca_bundle.pem', 'utf8'); // Opcional, se houver bundle de certificados intermediários
+
+const credentials = {
+  key: privateKey,
+  cert: certificate,
+};
+
 const corsOptions = {
   exposedHeaders: 'X-Total-Count',
 };
@@ -24,7 +36,7 @@ const swaggerOptions = {
     },
     servers: [
       {
-        url: 'https://localhost:3333',
+        url: 'https://localhost:3347', // Atualize a porta para 3347 para usar HTTPS
       },
     ],
   },
@@ -32,6 +44,7 @@ const swaggerOptions = {
 };
 
 const specs = swaggerJsDoc(swaggerOptions);
+
 app.use(cors(corsOptions));
 app.use(express.json());
 app.use(routes);
@@ -40,14 +53,16 @@ app.use(
   '/api-docs',
   swaggerUi.serve,
   swaggerUi.setup(specs, { explorer: true })
-  // https://localhost:3333/api-docs/#/ (URL para acessar documentação)
+  // https://localhost:3347/api-docs/#/ (URL para acessar documentação)
 );
 app.use('/', (req, res) => {
   return res.status(200).json({
     notification: 'Ok, server is running!',
   });
-})
+});
 
-app.listen(port, () => {
-  console.log(`Listening on port: ${port}`);
+const httpsServer = https.createServer(credentials, app);
+
+httpsServer.listen(port, () => {
+  console.log(`Listening on port (HTTPS): ${port}`);
 });
